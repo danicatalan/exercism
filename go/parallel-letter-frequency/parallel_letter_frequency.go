b09e9805d19eb64c66593e90817a30eec19cd901
Dani Catalan <danicatalan@hotmail.com>
2022-02-18 10:21:41 +0100

    ADD parallel letter frequency solution with sync

--- go/parallel-letter-frequency/parallel_letter_frequency.go
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,5 +1,7 @@
 package letter
 
+import "sync"
+
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
@@ -12,3 +14,38 @@ func Frequency(s string) FreqMap {
 	}
 	return m
 }
+
+// ConcurrentFrequency counts the frequency of each rune in a given text and
+// returns this data as a FreqMap.
+func ConcurrentFrequency(input []string) FreqMap {
+	m := FreqMap{}
+
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+
+	for i, s := range input {
+		wg.Add(1)
+		go func(s string, i int) {
+			newMap := Frequency(s)
+			mutex.Lock()
+			for k, v := range newMap {
+				m[k] += v
+			}
+			mutex.Unlock()
+			wg.Done()
+		}(s, i)
+	}
+	wg.Wait()
+
+	return m
+}
+
+/*
+benchmark
+	goos: darwin
+	goarch: amd64
+	pkg: letter
+	cpu: Intel(R) Core(TM) i5-8279U CPU @ 2.40GHz
+	BenchmarkSequentialFrequency-8   	   61051	     19291 ns/op
+	BenchmarkConcurrentFrequency-8   	   57841	     20922 ns/op
+*/
